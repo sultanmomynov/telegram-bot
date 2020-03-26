fbb0aa9b96031d25f423232614a315ed11ef7412
sultanmomynov <momynov.sultan@gmail.com>
2020-03-27 03:57:47 +0600

    init: add youtube to mp3 function

---
+++ index.js
@@ -0,0 +1,53 @@
+process.env["NTBA_FIX_319"] = 1;
+process.env["NTBA_FIX_350"] = 1;
+
+// Requirements
+const TelegramBot = require('node-telegram-bot-api')
+const config = require('config')
+const youtubedl = require('youtube-dl')
+const token = config.get('token')
+
+const bot = new TelegramBot(token, { polling: true });
+
+const linkRegex = /^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/
+const channel_id = config.get('channel_id')
+
+
+bot.on('message', (msg) => {
+
+  if (linkRegex.test(msg.text)) {
+
+    let url = msg.text
+    bot.sendMessage(channel_id, url);
+
+    const a = new Promise((resolve, reject) => {
+      console.log(`\nWorking...\n`);
+
+      const args = [
+        '-x',
+        '--audio-format',
+        'mp3',
+        '-o',
+        './tmp/%(id)s.%(ext)s'
+      ]
+
+      youtubedl.exec(url, args, {}, (err, out) => {
+        if (err) {
+          console.log(out.join('\n'))
+          bot.sendMessage(channel_id, 'Error occured')
+          throw err
+        }
+        console.log(out.join('\n'))
+        console.log(`\nDownload finished!\n`)
+        resolve(out);
+      })
+    })
+
+    a.then((data) => {
+      let kek = data[0].split(" ")[1].replace(':', '.mp3')
+      let file_name = './tmp/' + kek
+      console.log('Sending audio...');
+      bot.sendAudio(channel_id, file_name)
+    })
+  }
+});
